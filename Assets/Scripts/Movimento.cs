5572a822194f780b3d1d8fab8e897de25e49c742
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2019-10-14 22:45:27 -0300

    Adicionado movimento ao personagem

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public Vector3 alvo = new Vector3(5, 0, 4);
+    public float velocidade = 1f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(alvo.normalized * velocidade * Time.deltaTime);
+    }
+}
