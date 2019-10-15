2feaddf8fbc148a0bf8c19878e40b5aea507d6d1
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2019-10-15 00:11:29 -0300

    Adicionado movimento de seguir um alvo

---
+++ Assets/Scripts/MoveAoAlvo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAoAlvo : MonoBehaviour
+{
+    public float velocidade = 2.0f;
+    public float precisao = 0.01f;
+    public Transform alvo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(alvo.position);
+        Vector3 direcao = alvo.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, direcao, Color.red);
+        if (direcao.magnitude > precisao)
+        {
+            this.transform.Translate(direcao.normalized * velocidade * Time.deltaTime, Space.World);
+        }
+    }
+}
