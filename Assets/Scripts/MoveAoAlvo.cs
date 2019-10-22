0fc1924ce298ec05e3c880d1572a2c3a98f45b3e
Alexandre Kavalerski <kavalerskialexandre@gmail.com>
2019-10-22 00:34:51 -0300

    Atualizações e novas features

--- Assets/Scripts/MoveAoAlvo.cs
+++ Assets/Scripts/MoveAoAlvo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveAoAlvo : MonoBehaviour
 {
     public float velocidade = 2.0f;
-    public float precisao = 0.01f;
+    public float precisao = 1f;
     public Transform alvo;
     void Start()
     {
@@ -15,12 +15,16 @@ public class MoveAoAlvo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.LookAt(alvo.position);
+        Vector3 alvoParaOlhar = new Vector3(alvo.position.x,
+                                this.transform.position.y,
+                                alvo.position.z);
+        this.transform.LookAt(alvoParaOlhar);
         Vector3 direcao = alvo.position - this.transform.position;
         Debug.DrawRay(this.transform.position, direcao, Color.red);
-        if (direcao.magnitude > precisao)
+        if (Vector3.Distance(this.transform.position, alvo.position) > precisao)
         {
-            this.transform.Translate(direcao.normalized * velocidade * Time.deltaTime, Space.World);
+            //this.transform.Translate(direcao.normalized * velocidade * Time.deltaTime, Space.World);
+            this.transform.Translate(0, 0, velocidade * Time.deltaTime);
         }
     }
 }
---
+++ Assets/Scripts/TanqueController1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TanqueController1 : MonoBehaviour
+{
+    public UnityStandardAssets.Utility.WaypointCircuit circuito;
+    int indexAtual = 0;
+
+    public float velocidadeDeRotacao = 2f;
+    public float velocidade = 3.5f;
+    public float precisao = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (circuito.Waypoints.Length == 0) return;
+
+        Vector3 alvoParaOlhar = new Vector3(circuito.Waypoints[indexAtual].transform.position.x,
+                                        this.transform.position.y,
+                                        circuito.Waypoints[indexAtual].transform.position.z);
+        Vector3 direcao = alvoParaOlhar - this.transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                    Quaternion.LookRotation(direcao),
+                                    Time.deltaTime * velocidadeDeRotacao);
+
+        if(direcao.magnitude < precisao)
+        {
+            indexAtual++;
+            print("index: " + indexAtual);
+            if(indexAtual >= circuito.Waypoints.Length)
+            {
+                indexAtual = 0;
+            }
+        }
+        this.transform.Translate(0, 0, velocidade * Time.deltaTime);
+    }
+}
